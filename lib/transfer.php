30bfc92af8f0955fb128ce6ed0a15a86b5b6df75
emerada18 <emerada18@gmail.com>
2020-03-19 23:10:39 +0900

    Modified ipv4 and ipv6 filter

--- lib/transfer.php
+++ lib/transfer.php
@@ -1,14 +1,18 @@
 <?php
-$loc_ipv4 = filter_input(INPUT_GET, 'ipv4', FILTER_SANITIZE_URL);
 $loc_ipv6 = filter_input(INPUT_GET, 'ipv6', FILTER_SANITIZE_URL);
-
-if(strpos($loc_ipv4, ':')) {
-        header('HTTP/1.1 404');
-        exit;
+$loc_ipv4 = filter_input(INPUT_GET, 'ipv4', FILTER_SANITIZE_URL);
+if(strpos($loc_ipv4, ':') || strpos($loc_ipv6, ':')) {
+    header($_SERVER["SERVER_PROTOCOL"].'404');
+    exit;
 }
-//echo strlen($loc_ipv4); exit;
-if(!strlen($loc_ipv4)) {
-        header('HTTP/1.1 404');
-        exit;
+if(!strlen($loc_ipv4) || !strlen($loc_ipv6)) {
+    header($_SERVER["SERVER_PROTOCOL"].'404');
+    exit;
 }
-header("Location: $loc_ipv4", TRUE, 302);
+if ($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'HEAD') {
+    if (filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+        header("Location: $loc_ipv6", TRUE, 302);
+    } else {
+        header("Location: $loc_ipv4", TRUE, 302);
+    }
+}

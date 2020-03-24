781589e1fe05052a5788030d1fd87246d07f4d4b
emerada18 <emerada18@gmail.com>
2020-03-25 03:10:59 +0900

    Split the conditional expression as function

--- lib/redirect_by_addr_family.php
+++ lib/redirect_by_addr_family.php
@@ -1,12 +1,20 @@
 <?php
-$loc_ipv6 = filter_input(INPUT_GET, 'ipv6', FILTER_SANITIZE_URL);
 $loc_ipv4 = filter_input(INPUT_GET, 'ipv4', FILTER_SANITIZE_URL);
-if(strpos($loc_ipv4, ':') || strpos($loc_ipv6, ':')) {
+$loc_ipv6 = filter_input(INPUT_GET, 'ipv6', FILTER_SANITIZE_URL);
+
+function is_same_origin($ipv4, $ipv6) {
+    if (strpos($ipv4, ':') || strpos($ipv6, ':')){
+        return true;
+    }
+    return false;
+}
+
+if (is_same_origin($loc_ipv4, $loc_ipv6)) {
     header($_SERVER["SERVER_PROTOCOL"].'404');
     exit;
 }
-if(!strlen($loc_ipv4) || !strlen($loc_ipv6)) {
-    header($_SERVER["SERVER_PROTOCOL"].'404');
+if (!strlen($loc_ipv4) || !strlen($loc_ipv6)) {
+    header($_SERVER["SERVER_PROTOCOL"].'400');
     exit;
 }
 if ($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'HEAD') {

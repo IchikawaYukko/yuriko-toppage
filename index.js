fad73d9fe2ca10145adc93568a09fc04ab6fe522
市川ゆり子/IchikawaYuriko <ichikawayuriko@yahoo.co.jp>
2020-05-02 22:57:25 +0900

    add index.js
    Signed-off-by: 市川ゆり子/IchikawaYuriko <ichikawayuriko@yahoo.co.jp>

---
+++ index.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+    $("#nanjing").hover(
+    function () {
+        $(".yuriko").attr("src", "images/yuriko.png");
+    },
+    NOP()
+    );
+    $("#elehobby-ipv6").hover(
+    function () {
+        $(".yuriko").attr("src", "images/iya.png");
+    },
+    NOP()
+    );
+    $("#wikipedia").hover(
+    function () {
+        $(".yuriko").attr("src", "images/yuriko-fuyu.png");
+    },
+    NOP()
+    );
+    $("#osm").hover(
+    function () {
+        $(".yuriko").attr("src", "images/yuriko-natsu.png");
+    },
+    NOP()
+    );
+    $("#github").hover(
+    function () {
+        $(".yuriko").attr("src", "images/sailor.png");
+    },
+    NOP()
+    );
+    $(".yuriko").hover(
+    function () {
+        $(this).attr("src", "images/yuriko2.png");
+    },
+    NOP()
+    );
+    $("#bookmeter").hover(
+    function () {
+        $("#dontcry").html("あれ・・・？");
+        $("#yuriko").html("目からポカリが・・・");
+    },
+    NOP()
+    );
+    $("#oldsite").hover(
+    function () {
+        $("#dontcry").html("ぅーぅー");
+        $("#yuriko").html("しくしく(泣)");
+    },
+    NOP()
+    );
+});
+
+function NOP() {
+    //何もしない
+}
